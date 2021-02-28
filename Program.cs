172ea19d523a6efd00a47b76474bb572d1588018
Ryan Youngblood <rryb77@gmail.com>
2021-02-28 16:36:35 -0600

    Added Magic Moose (8ball) functionality, also added a menu for users to choose different games or exit

--- Program.cs
+++ Program.cs
@@ -7,14 +7,92 @@ void Main()
 {
     Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
     Console.WriteLine("--------------------------------------------");
+    Console.WriteLine("|                                          |");
+    Console.WriteLine("|             Choose an Option             |");
+    Console.WriteLine("|                                          |");
+    Console.WriteLine("| 1. Questions                             |");
+    Console.WriteLine("| 2. Magic Moose (8Ball)                   |");
+    Console.WriteLine("| 3. Rock, Paper, Scissors                 |");
+    Console.WriteLine("| 4. Exit                                  |");
+    Console.WriteLine("--------------------------------------------");
     Console.WriteLine();
+    Console.Write("Choice: ");
+    string answer = Console.ReadLine();
 
-    // Let the moose speak!
-    MooseSays("H I, I'M  E N T H U S I A S T I C !");
-    MooseSays("I really am enthusiastic");
+    if (String.IsNullOrWhiteSpace(answer))
+    {
+        Console.WriteLine("Invalid Choice! Please Choose again");
+        Console.Write("Choice: ");
+    }
+    else if (answer == "1")
+    {
+        Questions();
+    }
+    else if (answer == "2")
+    {
+        MagicMoose();
+    }
+    else if (answer == "3")
+    {
+
+    }
+    else if (answer == "4")
+    {
+        Environment.Exit(-1);
+    }
 
     // Ask a question
-    Questions();
+    // Questions();
+    MagicMoose();
+}
+
+void MagicMoose()
+{
+    List<string> MagicAnswers = new List<string>();
+    MagicAnswers.Add("As I see it, yes.");
+    MagicAnswers.Add("Ask again later.");
+    MagicAnswers.Add("Better not tell you now.");
+    MagicAnswers.Add("Cannot predict now.");
+    MagicAnswers.Add("Concentrate and ask again.");
+    MagicAnswers.Add("Don’t count on it.");
+    MagicAnswers.Add("It is certain.");
+    MagicAnswers.Add("It is decidedly so.");
+    MagicAnswers.Add("Most likely.");
+    MagicAnswers.Add("My reply is no.");
+    MagicAnswers.Add("My sources say no.");
+    MagicAnswers.Add("Outlook not so good.");
+    MagicAnswers.Add("Outlook good.");
+    MagicAnswers.Add("Reply hazy, try again.");
+    MagicAnswers.Add("Signs point to yes.");
+    MagicAnswers.Add("Very doubtful.");
+    MagicAnswers.Add("Without a doubt.");
+    MagicAnswers.Add("Yes.");
+    MagicAnswers.Add("Yes - definitely");
+    MagicAnswers.Add("You may rely on it");
+
+    bool playing = true;
+
+    while (playing)
+    {
+
+        Console.Write($"What question do you seek answers to? ");
+        string answer = Console.ReadLine();
+
+        Random r = new Random();
+        int genRand = r.Next(0, 19);
+
+        if (String.IsNullOrWhiteSpace(answer))
+        {
+            // Main();
+            playing = false;
+            Environment.Exit(-1);
+        }
+        else
+        {
+            MooseSays(MagicAnswers[genRand]);
+        }
+    }
+
 }
 
 
@@ -52,6 +130,24 @@ void Questions()
         }
 
         AnswerKey = AnswerKey + 2;
+    }
+
+    Console.Write("Go back to the main menu? (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write("Go back to the main menu? (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        Main();
+    }
+    else
+    {
+        Environment.Exit(-1);
     }
 
 }
